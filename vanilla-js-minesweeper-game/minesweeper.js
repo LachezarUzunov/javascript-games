49f6cb9ed476d999e8b664fed0ce219ef42bade5
Lachezar Uzunov <uzunov_89_lacho@abv.bg>
2022-09-21 04:06:44 +0300

    populating game grid and setting mines coordinates

--- vanilla-js-minesweeper-game/minesweeper.js
+++ vanilla-js-minesweeper-game/minesweeper.js
@@ -1,13 +1,31 @@
+const TILE_STATUSES = {
+  HIDDEN: "hidden",
+  MINE: "mine",
+  NUMBER: "number",
+  MARKED: "marked",
+};
+
 export function createBoard(boardSize, numberOfMInes) {
   const board = [];
+  const minePositions = getMinePositions(boardSize, numberOfMInes);
 
   for (let x = 0; x < boardSize; x++) {
     const row = [];
 
     for (let y = 0; y < boardSize; y++) {
+      const element = document.createElement("div");
+      element.dataset.status = TILE_STATUSES.HIDDEN;
       const tile = {
+        element,
         x,
         y,
+        mine: minePositions.some(positionMatch.bind(null, { x, y })),
+        get status() {
+          return this.element.dataset.status;
+        },
+        set status(value) {
+          this.element.dataset.status = value;
+        },
       };
       row.push(tile);
     }
@@ -15,4 +33,29 @@ export function createBoard(boardSize, numberOfMInes) {
   }
 
   return board;
+}
+
+function getMinePositions(boardSize, numberOfMines) {
+  const positions = [];
+
+  while (positions.length < numberOfMines) {
+    const position = {
+      x: randomNumber(boardSize),
+      y: randomNumber(boardSize),
+    };
+
+    if (!positions.some(positionMatch.bind(null, position))) {
+      positions.push(position);
+    }
+
+    return positions;
+  }
+}
+
+function positionMatch(a, b) {
+  return a.x === b.x && a.y === b.y;
+}
+
+function randomNumber(size) {
+  return Math.floor(Math.random() * size);
 }
--- vanilla-js-minesweeper-game/script.js
+++ vanilla-js-minesweeper-game/script.js
@@ -1,7 +1,25 @@
 // Display/ UI
+
+import { createBoard } from "./minesweeper.js";
+
+const BOARD_SIZE = 10;
+const NUMBER_OF_MINES = 10;
+
+const board = createBoard(BOARD_SIZE, NUMBER_OF_MINES);
+const boardElement = document.querySelector(".board");
+const minesLeftText = document.querySelector("[data-mine-count]");
+board.forEach((row) => {
+  row.forEach((tile) => {
+    boardElement.append(tile.element);
+  });
+});
+
+boardElement.style.setProperty("--size", BOARD_SIZE);
+minesLeftText.textContent = NUMBER_OF_MINES;
+
 // 1. Populate a board with tiles/mines
 // 2. Left click on tiles
-    // a. Reveal tiles
+// a. Reveal tiles
 // 3. Right click on tiles
 //  a. Mark tiles
 // 4. Check for win/lose
